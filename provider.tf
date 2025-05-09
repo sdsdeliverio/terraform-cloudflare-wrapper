0912916a1db0d7145faa6531674d04cb10d3e547
Ivor Smorenburg <ivor@smorenburg.me>
2025-05-09 23:20:31 +0100

    refactor: Move provider configuration to versions.tf for better organization

--- provider.tf
+++ provider.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "5.4.0"
-    }
-  }
-}
+
 
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
