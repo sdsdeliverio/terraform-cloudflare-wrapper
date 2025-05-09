4602fce4d2cac1b2b62290ad7fb79789c62c5617
Ivor Smorenburg <ivor@smorenburg.me>
2025-05-09 21:08:45 +0100

    Remove output definitions for module settings in outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "module_enabled" {
-  description = "Whether the module is enabled"
-  value       = var.module_enabled
-}
-
-output "module_tags" {
-  description = "Tags applied to the module's resources"
-  value       = var.module_tags
-}
