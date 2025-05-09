3c8d99c21e3cd6948c943575ebaa21b9eece379d
Ivor Smorenburg <ivor@smorenburg.me>
2025-05-09 23:28:32 +0100

    refactor: Remove redundant cloudflare provider blocks from provider.tf

--- provider.tf
+++ provider.tf
@@ -6,13 +6,3 @@ terraform {
     }
   }
 }
-
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
-
-# Configure provider alias for modules if needed
-provider "cloudflare" {
-  alias     = "main"
-  api_token = var.cloudflare_api_token
-}
