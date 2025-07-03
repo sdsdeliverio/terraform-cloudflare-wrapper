070925d6928c65d9998e5da98f108ea4fbed464c
Ivor Smorenburg <ivor@smorenburg.me>
2025-07-03 17:22:13 +0200

    fix: Remove is_default field from zero trust security module configuration for cleaner resource definition

--- modules/zero_trust_security/main.tf
+++ modules/zero_trust_security/main.tf
@@ -175,7 +175,6 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_virtual_network" "this" {
 
   account_id         = var.account_id
   name               = each.key
-  is_default         = try(each.value.is_default, false)
   is_default_network = try(each.value.is_default_network, false)
   comment            = try(each.value.comment, null)
 }
